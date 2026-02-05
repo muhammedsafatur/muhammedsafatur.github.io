4887c1cf964e9292080912b941da9859a8e26dca
Muhammed Safa Tur <44067638+muhammedsafatur@users.noreply.github.com>
2026-02-05 16:28:43 +0300

    Clean up comments in vite.config.ts
    Removed comments about GitHub Pages configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages'te repo adınıza göre burayı güncelleyin. 
-  // Örn: Repo adınız 'portfolio' ise base: '/portfolio/' olmalı.
-  // Eğer kullanıcı sitesi ise (kullaniciadi.github.io) '/' olarak bırakın.
+  
   base: './', 
 });
